5154c6f08789a3116d4599483afa58cdbdfa6709
Gosherama <macciak@gmail.com>
2019-03-04 11:44:43 +0100

    Added gallery section, styling and jquery for img filtering

--- script.js
+++ script.js
@@ -24,5 +24,18 @@ $(window).scroll(function() {
     $('.mission-text').removelass('fromRight');
     }
   });
+// Gallery filter
+$('.gallery-list-item').click(function() {
+  let value = $(this).attr('data-filter');
+  if(value === 'all') {
+    $('.filter').show(300);
+  } else {
+    $('.filter').not('.' + value).hide(300);
+    $('.filter').filter('.' + value).show(300);
+  }
+});
+$('.gallery-list-item').click(function() {
+  $(this).addClass('active-item').siblings().removeClass('active-item');
+});
 });
 
