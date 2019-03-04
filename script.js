4ab2a3b201751362259ac786e7a1da4b374f2a1a
Gosherama <macciak@gmail.com>
2019-03-04 11:01:33 +0100

    Added slide animation and jquery

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 // toggler effect
 $(document).ready(function() {
-    $('.nav-button').click(function() {
-        $('.nav-button').toggleClass('change');
-    });
+  $('.nav-button').click(function() {
+    $('.nav-button').toggleClass('change');
+  });
 
 // Navbar scrolldown
 $(window).scroll(function() {
-    let position = $(this).scrollTop();
-    if (position >= 200) {
-        $('.nav-menu').addClass('custom-navbar');
-    } else {
-        $('.nav-menu').removeClass('custom-navbar');
+  let position = $(this).scrollTop();
+  if (position >= 200) {
+    $('.nav-menu').addClass('custom-navbar');
+  } else {
+    $('.nav-menu').removeClass('custom-navbar');
+  }
+  });
+// Mission effect (slide right and left)
+$(window).scroll(function() {
+  let position = $(this).scrollTop();
+  if(position >= 650) {
+    $('.camera-img').addClass('fromLeft');
+    $('.mission-text').addClass('fromRight');
+  } else {
+    $('.camera-img').removeClass('fromLeft');
+    $('.mission-text').removelass('fromRight');
     }
-    }); 
+  });
 });
 
